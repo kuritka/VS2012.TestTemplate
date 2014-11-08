b2b4ee62c512eb85af44c5c01e042cb0f0f015f6
kuritka <kuritka@gmail.com>
2014-11-08 19:44:47 +0100

    VS2012 custom template with nuget packages  NUnit and FluentAssertions

---
+++ UnitTest1.PrivateMemebers.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace $safeprojectname$
+{
+    public partial class UnitTest1
+    {
+
+        [TestFixtureSetUp]
+        public void Init()
+        {
+        }
+
+        [TestFixtureTearDown]
+        public void Dispose()
+        {
+        }
+
+    }
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace $safeprojectname$
+{
+    [TestFixture]
+    partial class UnitTest1
+    {
+       
+        [Test]
+        public void TestMethod1()
+        {
+            //Method().Should().NotEndWith();
+        }
+
+        [Test]
+        public void TestMethod2()
+        {
+           // Assert.Throws<NotImplementedException>(() => XXX());
+        }
+
+    }
+}
